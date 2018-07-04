6f09643dbaa28362de7371ba616f950cc4c8ac23
Sarah Jacob <smjacob91@gmail.com>
2018-07-04 12:43:07 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -8,16 +8,7 @@ end
 
 
 def first_element(array)
-  
+  array(0)
+end
 
-context 'indexing' do
-    before(:each) do
-      @taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
-    end
-    
-    describe "#first_element" do
-      it "takes in an argument of an array and returns the first element in the array using its positive index" do
-        my_first_element = first_element(@taylor_swift)
-        expect(my_first_element).to eq("Welcome to New York")
-      end
-    end
+
