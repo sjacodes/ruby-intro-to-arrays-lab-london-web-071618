65807ca289d957a4cfdd6ce662e323de684eb385
Sarah Jacob <smjacob91@gmail.com>
2018-07-04 12:47:08 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -20,20 +20,11 @@ def last_element(array)
 end
 
 def first_element_with_array_methods(array)
-  
-    
-context 'using ruby array methods to return values from an array' do
-    before(:each) do
-      @south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
-    end
-    
-    describe "#first_element_with_array_methods" do 
-      it 'takes in an argument of an array and returns the first element in the array, without referencing the index number of that element' do
-        first_country = first_element_with_array_methods(@south_east_asia)
-        expect(first_country).to eq("Thailand")
-      end
-    end
+  array.first
+end
 
+def last_element_with_array_methods(array)
+  
     describe "#last_element_with_array_methods" do
       it 'takes in an argument of an array and returns the last element in the array, wihtout referencing the index number' do
         last_country = last_element_with_array_methods(@south_east_asia)
